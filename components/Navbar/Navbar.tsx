9fc8d0b0ca5aa245eae03b672bc331f117e70549
waelkhayati <khayatiwael@gmail.com>
2023-08-29 20:51:07 +0100

    commit

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import { Stack, Link } from '@chakra-ui/react'
-import styles from './navbar.module.css';
-
 export default function Navbar() {
   return (
     <>
-        <Stack className={styles.navbar} direction={"row-reverse"} >
+        <Stack direction={"row-reverse"} >
             <Link>Privacy Policy</Link>
             <Link>Contact</Link>
             <Link>Templates</Link>
